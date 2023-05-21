fa2e5a8477d1284ad30e1e8212929dd7e085eaca
Robbani571 <gr77rubel@gmail.com>
2023-05-21 17:49:39 +0600

    search by id route added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port = process.env.PORT || 5444;
+const port = process.env.PORT || 5000;
 const app = express();
 
 // middlewares
@@ -21,28 +22,108 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// const verifyJwt = (req, res, next) => {
+//   console.log('hitting verify jwt')
+//   console.log(req.headers.authorization)
+//   const authorization = req.headers.authorization;
+//   if (!authorization) {
+//     return res.status(401).send({ error: true, message: 'Unauthorized access' })
+//   }
+//   const token = authorization.split(' ')[1];
+//   console.log(token)
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+//     if (error) {
+//       return res.status(403).send({ error: true, message: 'Unauthorized access' })
+//     }
+//     else {
+//       req.decoded = decoded;
+//     }
+//   })
+//   next()
+// }
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
     const toyData = client.db('toyDB').collection('toys')
 
-    app.get('/products', async (req, res) => {
-      const result = await toyData.find().toArray()
+
+    app.post('/jwtToken', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      console.log(token)
+      res.send({ token })
+    })
+
+    app.post('/products', async (req, res) => {
+      const newToy = req.body;
+      console.log(newToy)
+      const result = await toyData.insertOne(newToy)
       res.send(result)
     })
 
-    app.get('/products/:id', async(req, res) => {
+    // get data by email
+    app.get('/products', async (req, res) => {
+
+      // const decoded = req.decoded;
+      // if(decoded.email !== req.query.email){
+      //   return res.status(403).send({error: true, message: 'Forbidden access'})
+      // }
+
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await toyData.find(query).toArray()
+      res.send(result)
+    })
+
+    // get data by product name
+    // app.get('/products', async(req, res) => {
+    //   console.log(req.query)
+    //   let query = {};
+    //   if (req.query?.name) {
+    //     query = { name: req.query.name }
+    //   }
+    //   const result = await toyData.find(query).toArray()
+    //   res.send(result)
+    // })
+
+    // get data for pagination
+    app.get('/products', async(req, res) => {
+      console.log(req.query)
+      
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = (page -1) * limit;
+      console.log(limit, skip)
+      const result = await toyData.find().skip(skip).limit(limit).toArray();
+      res.send(result)
+    })
+
+    // get data by product id
+    app.get('/products/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await toyData.findOne(query)
       res.send(result)
     })
+
     
+
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyData.deleteOne(query)
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
-     client.db("admin").command({ ping: 1 });
+    client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -54,9 +135,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('car is running')
+  res.send('car is running')
 })
 
 app.listen(port, () => {
-    console.log('car is running on road:', port)
+  console.log('car is running on road:', port)
 })
